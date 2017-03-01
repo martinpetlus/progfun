c7ca5f169beb947de14dc83c7911a6f2a2e78cc2
Martin Petluš <martinpetlus@gmail.com>
2017-03-01 21:33:47 +0100

    Implement quickEncode

--- week4/patmat/src/main/scala/patmat/Huffman.scala
+++ week4/patmat/src/main/scala/patmat/Huffman.scala
@@ -226,7 +226,8 @@ object Huffman {
    * This function returns the bit sequence that represents the character `char` in
    * the code table `table`.
    */
-  def codeBits(table: CodeTable)(char: Char): List[Bit] = ???
+  def codeBits(table: CodeTable)(char: Char): List[Bit] =
+    if (table.head._1 == char) table.head._2 else codeBits(table.tail)(char)
 
   /**
    * Given a code tree, create a code table which contains, for every character in the
@@ -236,14 +237,20 @@ object Huffman {
    * a valid code tree that can be represented as a code table. Using the code tables of the
    * sub-trees, think of how to build the code table for the entire tree.
    */
-  def convert(tree: CodeTree): CodeTable = ???
+  def convert(tree: CodeTree): CodeTable = tree match {
+    case Leaf(char, _) => List((char, Nil))
+    case Fork(left, right, _, _) => mergeCodeTables(convert(left), convert(right))
+  }
 
   /**
    * This function takes two code tables and merges them into one. Depending on how you
    * use it in the `convert` method above, this merge method might also do some transformations
    * on the two parameter code tables.
    */
-  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = ???
+  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable =
+    if (!a.isEmpty) (a.head._1, 0 :: a.head._2) :: mergeCodeTables(a.tail, b)
+    else if (!b.isEmpty) (b.head._1, 1 :: b.head._2) :: mergeCodeTables(a, b.tail)
+    else Nil
 
   /**
    * This function encodes `text` according to the code tree `tree`.
@@ -251,5 +258,7 @@ object Huffman {
    * To speed up the encoding process, it first converts the code tree to a code table
    * and then uses it to perform the actual encoding.
    */
-  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = ???
+  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] =
+    if (text.isEmpty) Nil
+    else codeBits(convert(tree))(text.head) ::: quickEncode(tree)(text.tail)
 }
--- week4/patmat/src/main/scala/patmat/Main.scala
+++ week4/patmat/src/main/scala/patmat/Main.scala
@@ -3,7 +3,12 @@ package patmat
 object Main extends App {
   import Huffman._
   val t1 = Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5)
+  val t2 = Fork(Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5), Leaf('d',4), List('a','b','d'), 9)
   println(encode(t1)("ab".toList))
   println(decode(t1, List(0, 1)))
   println(decodedSecret)
+  println(convert(t1))
+  println(convert(t2))
+  println(quickEncode(t1)("ab".toList))
+  println(quickEncode(t2)("abd".toList))
 }
