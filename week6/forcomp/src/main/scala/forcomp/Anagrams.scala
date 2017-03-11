30d8e68197271c7a34280e7b5db8cd638dc37394
Martin Petluš <martinpetlus@gmail.com>
2017-03-11 18:51:14 +0100

    Implement combinations

--- week6/forcomp/src/main/scala/forcomp/Anagrams.scala
+++ week6/forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -86,7 +86,18 @@ object Anagrams {
    *  Note that the order of the occurrence list subsets does not matter -- the subsets
    *  in the example above could have been displayed in some other order.
    */
-  def combinations(occurrences: Occurrences): List[Occurrences] = ???
+  def combinations(occurrences: Occurrences): List[Occurrences] = {
+    if (occurrences.isEmpty)
+      List(List())
+    else {
+      val ll = combinations(occurrences.tail)
+
+      (for {
+        l <- ll
+        c <- 1 to occurrences.head._2
+      } yield (occurrences.head._1, c) :: l) ++ ll
+    }
+  }
 
   /** Subtracts occurrence list `y` from occurrence list `x`.
    *
