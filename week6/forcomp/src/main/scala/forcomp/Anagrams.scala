5e09bd8c91316012649b49c7b294bd9a086caa4f
Martin Petluš <martinpetlus@gmail.com>
2017-03-11 14:05:39 +0100

    Implement first two methods

--- week6/forcomp/src/main/scala/forcomp/Anagrams.scala
+++ week6/forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -34,10 +34,15 @@ object Anagrams {
    *
    *  Note: you must use `groupBy` to implement this method!
    */
-  def wordOccurrences(w: Word): Occurrences = ???
+  def wordOccurrences(w: Word): Occurrences = {
+    val m = w groupBy (_.toLower)
+    val p = m map {case (key: Char, value: String) => (key, value.length)}
+    p.toList.sortWith (_._1 < _._1)
+  }
 
   /** Converts a sentence into its character occurrence list. */
-  def sentenceOccurrences(s: Sentence): Occurrences = ???
+  def sentenceOccurrences(s: Sentence): Occurrences =
+    wordOccurrences (s mkString "")
 
   /** The `dictionaryByOccurrences` is a `Map` from different occurrences to a sequence of all
    *  the words that have that occurrence count.
