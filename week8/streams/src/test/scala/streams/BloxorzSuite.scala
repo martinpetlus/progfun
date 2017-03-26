9f08763fdb05af09d6ef2928c32d1db19bec9994
Martin Petluš <martinpetlus@gmail.com>
2017-03-26 22:41:58 +0200

    Implement and test newNeighborsOnly

--- week8/streams/src/test/scala/streams/BloxorzSuite.scala
+++ week8/streams/src/test/scala/streams/BloxorzSuite.scala
@@ -74,6 +74,21 @@ class BloxorzSuite extends FunSuite {
     }
   }
 
+  test("newNeighborsOnly") {
+    new Level1 {
+      val s = newNeighborsOnly(
+        Set(
+          (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+          (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+        ).toStream,
+        Set(Block(Pos(1,2),Pos(1,3)), Block(Pos(1,1),Pos(1,1)))
+      )
+      assert(s == Set(
+        (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+      ).toStream)
+    }
+  }
+
 	test("optimal solution for level 1") {
     new Level1 {
       assert(solve(solution) == Block(goal, goal))
