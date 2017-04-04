4ad40304d06c2ab306b2c67eb6cad99d4776877f
Martin Petluš <martinpetlus@gmail.com>
2017-04-04 20:41:28 +0200

    Add min1 prop

--- week9/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ week9/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -19,6 +19,11 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
+  property("min1") = forAll { a: Int =>
+    val h = insert(a, empty)
+    findMin(h) == a
+  }
+
   property("gen1") = forAll { (h: H) =>
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
